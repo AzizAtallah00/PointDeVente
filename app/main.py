41e28f7c655fea56f2be9bd65657976c77c93bd6
Mohamed Aziz Atallah <mohamedaziz.atallah@enicar.ucar.tn>
2024-10-07 22:24:43 +0100

    fix session routing

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from app.routers import employee
+from app.routers import employee, session
 
 app = FastAPI()
 
 app.include_router(employee.router)
+app.include_router(session.router)
 
 
 
